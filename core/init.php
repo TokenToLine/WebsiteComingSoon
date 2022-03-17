08d16316f78a411be26a4f2e1741843c247f81cc
TokenToLine <tokentoline@gmail.com>
2022-03-17 17:13:30 +0100

    Added database connection files

---
+++ core/init.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "empty for security reasons";
+$username = "empty for security reasons";
+$password = "empty for security reasons";
+$dbtabel = "empty for security reasons";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbtabel);
---
+++ core/insert.php
@@ -0,0 +1,8 @@
+<?php
+require 'init.php';
+
+// empty for security reasons
+
+// Close connection
+mysqli_close($conn);
+?>
