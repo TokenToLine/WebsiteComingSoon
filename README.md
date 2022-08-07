1f8acd5067c0ca509be7827dc9d166204566dab0
TokenToLine <tokentoline@gmail.com>
2022-08-07 20:46:28 +0200

    Old code remove

--- README.md
+++
@@ -1,3 +0,0 @@
-# Website Coming Soon
-
-Old coming soon website. Down since 07-07-2022
--- core/insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'init.php';
-
-// empty for security reasons
-
-// Close connection
-mysqli_close($conn);
-?>
